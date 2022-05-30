de25e0a89129bb383e38219d2cab9c0b7f12edc3
Aniket Bhattacharjee <appsoftlimited@gmail.com>
2022-05-30 14:06:23 +0530

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -23,11 +23,6 @@ module.exports = [{
 • 2000 credits]
 $addField[1;Claim Rewards;Use \`$getServerVar[prefix]claim\` to claim rewards!]
   `} , {
-    name:"vote",
-    code:`$color[1;1d9e85]
-    $title[1;Voting!]
-    $description[1;[Vote Here!](https://top.gg/bot/731192650794926191/vote)]`
-  } , {
     name:"claim",
   $if:"v4",
     code:`$if[$httpRequest[https://normal-api.ml/topgg/hasvoted?bot=731192650794926191&user=$authorID&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjczMTE5MjY1MDc5NDkyNjE5MSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjI5MTMzNjQ0fQ.SiFT8WSX8Zs12fTweMrHPh-FqzTTkLIbiBOGkXMbhac;GET;;voted]==true]
